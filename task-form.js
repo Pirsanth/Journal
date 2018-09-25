deebb99ecd33d4a11276f80c3887f4610405e37f
Pirsanth <pirsanth@hotmail.com>
2018-09-25 19:10:21 +0100

    Added task-form date selection functionality and improved layout with flexbox. Yet to finish submit button and form validation

--- task-form.js
+++ task-form.js
@@ -3,6 +3,15 @@ const TASK_FORM_CANCEL_TASK_BUTTON = '[data-task-form = "cancel-task"]';
 const TASK_FORM_CALENDAR_CONTAINER = '[data-task-form="calendar-container"]';
 const TASK_FORM_CHANGE_DATE_BUTTON = '[data-task-form="change-button"]';
 
+const TASK_FORM_CALENDAR_CLICKABLE = '[data-task-form-calendar="clickable"]';
+const FORM_CONTROLS_COLLECTION = document.forms[0].elements;
+
+let formCalendar = document.querySelector(TASK_FORM_CALENDAR_CONTAINER);
+
+appendTaskFormCalendar();
+addChangeDateHandler();
+addClickableHandler();
+addFormSubmitHandler();
 
 function appendTaskFormCalendar() {
   let element = document.querySelector('[data-task-form="calendar"]');
@@ -11,4 +20,38 @@ function appendTaskFormCalendar() {
   let context = new monthObject(08, 2018);
   element.innerHTML = compiledFunction(context)
 }
-appendTaskFormCalendar();
+
+
+function addChangeDateHandler() {
+  let changeButton = document.querySelector(TASK_FORM_CHANGE_DATE_BUTTON);
+
+changeButton.addEventListener("click", function (event) {
+      formCalendar.classList.toggle("expand");
+});
+}
+
+function addClickableHandler(){
+  let clickableNodeList = document.querySelectorAll(TASK_FORM_CALENDAR_CLICKABLE);
+
+  for(i=0; i<clickableNodeList.length; i++){
+        clickableNodeList[i].addEventListener("click", function (event) {
+        FORM_CONTROLS_COLLECTION[1].value = event.target.textContent;
+        formCalendar.classList.toggle("expand");
+        });
+  }
+}
+
+function addFormSubmitHandler(){
+    let submitButton = document.querySelector(TASK_FORM_ADD_TASK_BUTTON);
+
+    submitButton.addEventListener("click", function (event) {
+      let serializedForm = {};
+
+              for(i=0; i<FORM_CONTROLS_COLLECTION.length; i++){
+                    if(i<2){
+                    }
+              }
+      document.forms[0].reset();
+    });
+
+}

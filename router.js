30d958c48bd748829470a4a48a40c9ed31a76c53
Pirsanth <pirsanth@hotmail.com>
2018-10-17 15:57:32 +0800

    Fixed bug in router.js where it would not respond to static file requests unless url was localhost:8080/user/current

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const {URL} = require("url");
 const fs = require("fs");
 const handlebars = require("handlebars");
 
-const cssFile = /^\/user\/((styles|task-form|normalize)\.css)$/;
-const jsFile = /^\/user\/((base|task-form|main)\.js)$/
+const cssFile = /^\/(\w+)\/((styles|task-form|normalize)\.css)$/;
+const jsFile = /^\/(\w+)\/((base|task-form|main)\.js)$/
 const currrentMonth = /^\/(\w+)\/current$/
 
 //\w means a-z, A-Z, 0-9, including the _ (underscore) character.
@@ -14,9 +14,9 @@ module.exports.requestHandler = function (req, res) {
         let url = new URL(req.url, "http://localhost:8080");
         let path = url.pathname;
       if(req.method === "GET"){
-          if(path.match(jsFile)|| path.match(cssFile)){
-              let fileRequested = path.match(jsFile)? path.match(jsFile)[1]: path.match(cssFile)[1];
-              require("./controller/serveStaticFile.js")(fileRequested, res);
+          if(jsFile.test(path)|| cssFile.test(path)){
+            let fileRequested = path.match(jsFile)? path.match(jsFile)[2]: path.match(cssFile)[2];
+            require("./controller/serveStaticFile.js")(fileRequested, res);
           }
           else if(path.match(currrentMonth)){
             let user = path.match(currrentMonth)[1];
@@ -33,25 +33,4 @@ module.exports.requestHandler = function (req, res) {
 
 
 // GET, POST, DELETE, PUT
-/*
-fun();
-
-function fun() {
-  require("./views/registerHandlebarHelpers.js").run();
-  var constructorFunction = require("./model/createNewDataModel.js").run
-  var obj = new constructorFunction(7, 2018);
-  var strang = "{{#task-form-calendar this}}<td {{{class}}} {{{data}}} > {{value}}</td>{{/task-form-calendar}}";
-  var funny = handlebars.compile(strang);
-  c(funny(obj));
-}
-
-*/
-//    var e = new Error(msg);
-
-//do not forget to strinigfy if you are sending JSON over the wire
-
-//dont forget that we have to chain the res after the "end" event
-//of the request stream
-//using callbacks
-
 //http://localhost:8080/user/current

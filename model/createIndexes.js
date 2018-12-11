ac838843df3b8ba1b5f1925002cf8b60c16066f7
Pirsanth <pirsanth@hotmail.com>
2018-12-11 09:43:12 +0800

    Changed the index on sessions to index the sessionId instead of username and hence removed the unique constraint on username. This would allow a user to login on multiple devices at the same time for a 3 hour period

--- model/createIndexes.js
+++ model/createIndexes.js
@@ -13,18 +13,18 @@ module.exports = function (fn) {
               fn(err);
               return;
             }
-            //usernames are unique
-            collection.createIndex({username: 1}, {unique: true}, function (err, result) {
-              if(err){
-                fn(err);
-                return;
-              }
-              indexA = true;
+                  //usernames are unique
+                  collection.createIndex({username: 1}, {unique: true}, function (err, result) {
+                    if(err){
+                      fn(err);
+                      return;
+                    }
+                    indexA = true;
 
-              if(indexA && indexB && indexC && indexD){
-                fn(null);
-              }
-            });
+                    if(indexA && indexB && indexC && indexD){
+                      fn(null);
+                    }
+                  });
           });
 
           db.collection("taskObjects", function (err, collection) {
@@ -32,50 +32,54 @@ module.exports = function (fn) {
               fn(err);
               return;
             }
-            collection.createIndex({username: 1, startUTCDate: 1, endUTCDate: 1, taskName: 1}, function (err, result) {
-                if(err){
-                  fn(err);
-                  return;
-                }
-                indexB = true;
+                  collection.createIndex({username: 1, startUTCDate: 1, endUTCDate: 1, taskName: 1}, function (err, result) {
+                      if(err){
+                        fn(err);
+                        return;
+                      }
+                      indexB = true;
 
-                if(indexA && indexB && indexC && indexD){
-                  fn(null);
-                }
-            });
-          })
+                      if(indexA && indexB && indexC && indexD){
+                        fn(null);
+                      }
+                  });
+          });
 
           db.collection("sessions", function (err, collection) {
             if(err){
               fn(err);
               return;
             }
-            //usernames are unique
-            collection.createIndex({username: 1}, {unique: true}, function (err, result) {
-              if(err){
-                fn(err);
-                return;
-              }
-              indexC = true;
+                    //I did not impose a unique username constraint so as to allow each user to login in on multiple devices at the same time
+                    /*Makes the session expire in 3 hours. Partly adresses the case where the user decides to delete the cookie on the client
+                     and login multiple times thereby polluting the sessions collection with Ids that will never be used. (More work might need to be
+                     done to defend against this attack?)*/
+                     //made the index on sessionId as it is more selective vs username
+                    collection.createIndex({sessionId: 1}, function (err, result) {
+                      if(err){
+                        fn(err);
+                        return;
+                      }
+                      indexC = true;
 
-              if(indexA && indexB && indexC && indexD){
-                fn(null);
-              }
-            });
+                      if(indexA && indexB && indexC && indexD){
+                        fn(null);
+                      }
+                    });
 
 
-            const threeHourExpiration = 60*60*3
-            collection.createIndex({dateLoggedIn: 1}, {expireAfterSeconds: threeHourExpiration}, function (err, result) {
-              if(err){
-                fn(err);
-                return;
-              }
-              indexD = true;
-              
-              if(indexA && indexB && indexC && indexD){
-                fn(null);
-              }
-            });
+                    const threeHourExpiration = 60*60*3
+                    collection.createIndex({dateLoggedIn: 1}, {expireAfterSeconds: threeHourExpiration}, function (err, result) {
+                      if(err){
+                        fn(err);
+                        return;
+                      }
+                      indexD = true;
+
+                      if(indexA && indexB && indexC && indexD){
+                        fn(null);
+                      }
+                    });
           });
     });
 

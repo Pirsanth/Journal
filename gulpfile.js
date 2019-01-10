bd365f282f514473b4fcdb4af87a2562926d59a6
Pirsanth <pirsanth@hotmail.com>
2019-01-10 19:58:33 +0800

    I learnt that the babel option useBuiltIns:"usage" is experimental and have decided to switch to useBuiltIns:"entry" to include all the polyfills for the target environment regardless of whether my code uses it. I have also included a polyfill-import.js file in the src folders containing only the babel polyfill import statement. Modified the src globs in gulpfile so that polyfill-import.js is the first file and main.js is the last file. Added a default gulp task that builds files, then serves them.

--- gulpfile.js
+++ gulpfile.js
@@ -12,25 +12,27 @@ const cssnano = require('cssnano');
 const source = require('vinyl-source-stream');
 const exorcist = require('exorcist');
 const fs = require('fs');
-
 var bs = require("browser-sync").create();
 var nodemon = require("gulp-nodemon");
 
-const homePageJsSrcGlob = ["./src/homePage/js/*.js", "!./src/homePage/js/main.js", "./src/homePage/js/main.js"];
-const loginPageJsSrcGlob = ["./src/loginPage/js/*.js", "!./src/loginPage/js/main.js", "./src/loginPage/js/main.js"];
+const homePageJsSrcGlob = ["./src/homePage/js/polyfill-import.js", "./src/homePage/js/*.js", "!./src/homePage/js/main.js", "./src/homePage/js/main.js"];
+const loginPageJsSrcGlob = ["./src/loginPage/js/polyfill-import.js", "./src/loginPage/js/*.js", "!./src/loginPage/js/main.js", "./src/loginPage/js/main.js"];
 const tempDir = "./temp";
 const homePageCssSrcGlob = ["./src/homePage/css/normalize.css", "./src/homePage/css/styles.css", "./src/homePage/css/task-form.css"];
 const loginPageCssSrcGlob = "./src/loginPage/css/login.css";
+//the dir for the build files have been hardcoded to dist below
 
 //factory function so the logic handling the login and home page's js and css is only written once
 //named the functions inside as well so they would show up on the command line
+
+
 function concatAndTranspile (srcGlob, pageName) {
   return function concatAndTranspile () {
           return  gulp.src(srcGlob)
                   .pipe(sourcemaps.init())
                   .pipe(concat("temp.js"))
                   .pipe(babel({"presets": [
-                    ["@babel/preset-env",{"useBuiltIns": "usage"}]
+                    ["@babel/preset-env",{"useBuiltIns": "entry"}]
                   ]
                 }))
                 .pipe(sourcemaps.write())
@@ -86,23 +88,19 @@ gulp.task("build", gulp.series(gulp.parallel("build:homePageJS", "build:homePage
                                deleteTempFiles)
           );
 
-//gulp.task("build", gulp.parallel())
-gulp.task("startServer", function () {
-  return  nodemon({"ignore": "assets/*", nodeArgs: ["--inspect"]});
+gulp.task("serve", function (cb) {
+  nodemon({"ignore": "./*", nodeArgs: ["--inspect"], stdout: false }).on("stdout", function (childBuffer) {
+      const childLog = childBuffer.toString();
+
+      console.log(childLog);
+
+      if(childLog.includes("Connection to mongodb established and the http server has been started on port 8080")){
+        bs.init({proxy: "http://localhost:8080/login.html",
+                port: 3000});
+        cb();
+      }
+
+  })
 });
 
-gulp.task("frontend",gulp.series("startServer", function () {
-      bs.init({proxy: "http://localhost:8080/login.html",
-              port: 3000})
-
-      gulp.watch("assets/*", bs.reload);
-
-}));
-
-gulp.task("login",gulp.series("startServer", function () {
-      bs.init({proxy: "http://localhost:8080/login.html",
-              port: 3000})
-
-      gulp.watch("controller/*", bs.reload);
-
-}));
+gulp.task("default", gulp.series("build", "serve"));
